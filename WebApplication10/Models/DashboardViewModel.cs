fe080f4c8f943bc31e04d917e836fa93407774e3
RAJENDRAN N <nrajendran497@gmail.com>
2025-12-17 12:53:56 +0530

    A new page for query selection checking

---
+++ WebApplication10/Models/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication10.Models
+{
+    public class DashboardViewModel
+    {
+        public string ReleaseVersion { get; set; } = "v2.4.1 - Live";
+        public int PendingCRFCount { get; set; } = 18;
+        public int TestCasesCount { get; set; } = 156;
+        public int OpenBugsCount { get; set; } = 8;
+        // You can add more dynamic values later (e.g., fetched from DB)
+    }
+}
